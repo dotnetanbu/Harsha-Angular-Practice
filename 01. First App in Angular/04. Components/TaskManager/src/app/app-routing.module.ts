045506b0480fe0824d98ffbe096eac3c7f0fd176
Anbarasan <anbarasansas@gmail.com>
2023-09-09 20:54:44 +0530

    Revert "Basic routing code added"
    This reverts commit fb4a67f99c7856c9a00631fc66eb8cc70b0b82d9.

--- 01. First App in Angular/04. Components/TaskManager/src/app/app-routing.module.ts
+++ 01. First App in Angular/04. Components/TaskManager/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [
-  { path:"dashboard", component:DashboardComponent },
-  { path:"about", component:AboutComponent },
-  { path:"", redirectTo:"dashboard", pathMatch:"full" }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
