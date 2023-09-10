e24507cf60ebf8629caa4e9122ba229bbc8bc3c7
Anbarasan <anbarasansas@gmail.com>
2023-09-10 15:32:48 +0530

    Dashboard Component Code changes

--- 02. Data Bindings/01. Dashboard/TaskManager/src/app/dashboard/dashboard.component.ts
+++ 02. Data Bindings/01. Dashboard/TaskManager/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  Designation: string = "";
+  Username: string = "";
+  NoOfTeamMembers: number = 0;
+  TotalCostOfAllProjects: number = 0;
+  PendingTasks: number = 0;
+  UpComingProjects: number = 0;
+  ProjectCost: number = 0;
+  CurrentExpenditure: number = 0;
+  AvailableFunds: number = 0; 
+
+  Clients: string[] = [];
+  Projects: string[] = [];
+  Years: number[] = [];
+  TeamMembersSummary: any = [];
+  TeamMembers: any = [];
+
+  ngOnInit(): void {
+    this.Designation = "Team Leader";
+    this.Username = "Anbu";
+    this.NoOfTeamMembers = 10;
+    this.TotalCostOfAllProjects = 20;
+    this.PendingTasks = 90;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 96788;
+    this.AvailableFunds = 52536;
+
+  }
 }
