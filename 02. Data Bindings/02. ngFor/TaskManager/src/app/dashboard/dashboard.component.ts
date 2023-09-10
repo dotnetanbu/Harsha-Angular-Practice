b7c2a2088b11587e2a656ffaf7c65bff429915c2
Anbarasan <anbarasansas@gmail.com>
2023-09-10 17:30:43 +0530

    nested ngFor Logic added for Team Members

--- 02. Data Bindings/02. ngFor/TaskManager/src/app/dashboard/dashboard.component.ts
+++ 02. Data Bindings/02. ngFor/TaskManager/src/app/dashboard/dashboard.component.ts
@@ -67,5 +67,44 @@ export class DashboardComponent implements OnInit {
         TemporarilyUnavailableMembers: 6,
       },
     ];
+
+    this.TeamMembers = [
+      {
+        Region: 'East',
+        Members: [
+          { ID: 1, Name: 'Ford', Status: 'Available' },
+          { ID: 2, Name: 'Miller', Status: 'Available' },
+          { ID: 3, Name: 'Jones', Status: 'Busy' },
+          { ID: 4, Name: 'James', Status: 'Busy' },
+        ],
+      },
+      {
+        Region: 'West',
+        Members: [
+          { ID: 5, Name: 'Anna', Status: 'Available' },
+          { ID: 6, Name: 'Arun', Status: 'Available' },
+          { ID: 7, Name: 'Varun', Status: 'Busy' },
+          { ID: 8, Name: 'Jasmine', Status: 'Busy' },
+        ],
+      },
+      {
+        Region: 'South',
+        Members: [
+          { ID: 9, Name: 'Krishna', Status: 'Available' },
+          { ID: 10, Name: 'Mohan', Status: 'Available' },
+          { ID: 11, Name: 'Raju', Status: 'Busy' },
+          { ID: 12, Name: 'Farooq', Status: 'Busy' },
+        ],
+      },
+      {
+        Region: 'North',
+        Members: [
+          { ID: 13, Name: 'Jacob', Status: 'Available' },
+          { ID: 14, Name: 'Smith', Status: 'Available' },
+          { ID: 15, Name: 'Jones', Status: 'Busy' },
+          { ID: 16, Name: 'James', Status: 'Busy' },
+        ],
+      },
+    ];
   }
 }
