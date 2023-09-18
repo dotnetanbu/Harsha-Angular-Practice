b2436ab74aa349c54ec3e5684070b277d71295b1
Anbarasan <anbarasansas@gmail.com>
2023-09-18 20:29:18 +0530

    RxJs Map Operation code changes added - Angular

--- 04. REST API Calls/06. RxJS Observable Map/TaskManager/src/app/projects.service.ts
+++ 04. REST API Calls/06. RxJS Observable Map/TaskManager/src/app/projects.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Project } from './project';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,16 @@ export class ProjectsService {
   constructor(private httpClient: HttpClient) {}
 
   getAllProjects(): Observable<Project[]> {
-    return this.httpClient.get<Project[]>(this.urlPrefix + 'Projects');
+    return this.httpClient
+      .get<Project[]>(this.urlPrefix + 'Projects', { responseType: 'json' })
+      .pipe(
+        map((data: Project[]) => {
+          for (let i = 0; i < data.length; i++) {
+            data[i].teamSize = data[i].teamSize * 100;
+          }
+          return data;
+        })
+      );
   }
 
   insertProject(newProject: Project): Observable<Project> {
