65e800c632a5e7dd2bb14843a8c32a0e2fd8ea78
Anbarasan <anbarasansas@gmail.com>
2023-09-18 20:19:47 +0530

    Initial Commit - Angular

---
+++ 04. REST API Calls/06. RxJS Observable Map/TaskManager/src/app/projects.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from './project';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProjectsService {
+  urlPrefix: string = 'http://localhost:5201/api/'; //Dot Net Core WebAPI URL
+
+  constructor(private httpClient: HttpClient) {}
+
+  getAllProjects(): Observable<Project[]> {
+    return this.httpClient.get<Project[]>(this.urlPrefix + 'Projects');
+  }
+
+  insertProject(newProject: Project): Observable<Project> {
+    return this.httpClient.post<Project>(
+      this.urlPrefix + 'Projects',
+      newProject
+    );
+  }
+
+  updateProject(existingProject: Project): Observable<Project> {
+    return this.httpClient.put<Project>(
+      this.urlPrefix + 'projects',
+      existingProject,
+      { responseType: 'json' }
+    );
+  }
+
+  deleteProject(projectID: number): Observable<string> {
+    return this.httpClient.delete<string>(
+      this.urlPrefix + 'projects/' + projectID.toString()
+    );
+  }
+
+  SearchProjects(searchBy: string, searchText: string): Observable<Project[]> {
+    return this.httpClient.get<Project[]>(
+      this.urlPrefix + 'projects/' + searchBy + '/' + searchText
+    );
+  }
+}
